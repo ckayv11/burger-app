e6c25e44a34feb3d84e8d24c1f5dea567810ada5
CKayV11 <11303975+ckayv11@users.noreply.github.com>
2019-05-29 17:29:52 -0500

    setup config, model, and controller files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,28 @@
+var express = require("express");
+
+var router = express.Router();
+
+// Import the model to use its database functions.
+var cat = require("../models/burger.js");
+
+// Create all routes and set up logic within those routes where required.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
